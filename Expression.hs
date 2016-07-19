8c9421c19b2cb816a50b5ad882144e271bc6cb3c
Gabriel Alves <itsmealves@gmail.com>
2016-07-18 22:59:34 -0300

    Supports floating-point numbers and greater than 10

--- Expression.hs
+++ Expression.hs
@@ -1,9 +1,9 @@
-module Expression(Expression(..), evaluate, fromPrefixNotation) where
+-- module Expression(Expression(..), evaluate, fromPrefixNotation) where
 
 import Data.Char
 
 data Operation = Add | Subtract | Multiply | Divide deriving (Show, Eq)
-data Expression = Empty | Value Int | Node Operation Expression Expression deriving (Show, Eq)
+data Expression = Empty | Value Float | Node Operation Expression Expression deriving (Show, Eq)
 
 precedence :: Char -> Integer
 precedence '+' = 1
@@ -11,17 +11,24 @@ precedence '-' = 1
 precedence '*' = 2
 precedence '/' = 2
 
-plant :: String -> [Expression]
+hasFloat :: String -> Bool
+hasFloat s = not $ null $ (reads s :: [(Float,String)])
+
+extractFloat :: String -> Float
+extractFloat s = fst $ head (reads s :: [(Float,String)])
+
+plant :: [String] -> [Expression]
 plant [] = []
 plant (h:t)
-    | isDigit h = (Value $ digitToInt h):plant t
-    | otherwise = (Node (toOperation h) Empty Empty):plant t
+    | hasFloat h = (Value $ extractFloat h):plant t
+    | otherwise = (Node (toOperation $ head h) Empty Empty):plant t
     where toOperation c
             | c == '+' = Add
             | c == '-' = Subtract
             | c == '*' = Multiply
             | c == '/' = Divide
             | otherwise = error "Unsupported operation"
+
 
 hasEmpty :: Expression -> Bool
 hasEmpty Empty = True
@@ -36,15 +43,15 @@ insertInto (Node o l r) item
     | hasEmpty r = Node o l (insertInto r item)
     | otherwise = (Node o l r)
 
-evaluate :: Expression -> Int
+evaluate :: Expression -> Float
 evaluate (Value x) = x
 evaluate (Node Add x y) = evaluate x + evaluate y
 evaluate (Node Subtract x y) = evaluate x - evaluate y
 evaluate (Node Multiply x y) = evaluate x * evaluate y
-evaluate (Node Divide x y) = evaluate x `quot` evaluate y
+evaluate (Node Divide x y) = evaluate x / evaluate y
 evaluate Empty = error "This is an incomplete expression"
 
 fromPrefixNotation :: String -> Expression
-fromPrefixNotation s = foldl (\acc e -> insertInto acc e) Empty $ plant s
+fromPrefixNotation s = foldl (\acc e -> insertInto acc e) Empty $ plant $ words s
 
 
