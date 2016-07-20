20f4d193701d37bc5817ade59a18c08fdefdb5c1
Gabriel Alves <itsmealves@gmail.com>
2016-07-19 23:08:41 -0300

    remove unnecessary imports and change precedence input type

--- Calculate/Expression.hs
+++ Calculate/Expression.hs
@@ -1,15 +1,29 @@
-module Calculate.Expression(Expression(..), evaluate, fromPrefixNotation, fromPostfixNotation) where
-
-import Data.Char
+module Calculate.Expression(Expression(..), 
+    evaluate, 
+    fromPrefixNotation, 
+    fromPostfixNotation, 
+    fromInfixNotation) 
+where
 
 data Operation = Add | Subtract | Multiply | Divide deriving (Show, Eq)
 data Expression = Empty | Value Float | Node Operation Expression Expression deriving (Show, Eq)
 
-precedence :: Char -> Integer
-precedence '+' = 1
-precedence '-' = 1
-precedence '*' = 2
-precedence '/' = 2
+precedence :: Operation -> Integer
+precedence Add = 1
+precedence Subtract = 1
+precedence Multiply = 2
+precedence Divide = 2
+
+insertInto :: Expression -> Expression -> Expression
+insertInto Empty item = item
+insertInto (Value x) _ = Value x
+insertInto (Node o l r) item
+    | hasEmpty l = Node o (insertInto l item) r
+    | hasEmpty r = Node o l (insertInto r item)
+    | otherwise = (Node o l r)
+    where hasEmpty Empty = True
+          hasEmpty (Value _) = False
+          hasEmpty (Node _ l r) = (hasEmpty l) || (hasEmpty r)
 
 plant :: [String] -> [Expression]
 plant [] = []
@@ -25,17 +39,6 @@ plant (h:t)
             | c == '/' = Divide
             | otherwise = error "Unsupported operation"
 
-insertInto :: Expression -> Expression -> Expression
-insertInto Empty item = item
-insertInto (Value x) _ = Value x
-insertInto (Node o l r) item
-    | hasEmpty l = Node o (insertInto l item) r
-    | hasEmpty r = Node o l (insertInto r item)
-    | otherwise = (Node o l r)
-    where hasEmpty Empty = True
-          hasEmpty (Value _) = False
-          hasEmpty (Node _ l r) = (hasEmpty l) || (hasEmpty r)
-
 evaluate :: Expression -> Float
 evaluate (Value x) = x
 evaluate (Node Add x y) = evaluate x + evaluate y
@@ -47,14 +50,8 @@ evaluate Empty = error "This is an incomplete expression"
 generate :: [Expression] -> Expression
 generate s = foldl (\acc e -> insertInto acc e) Empty s
 
-prepareInput :: String -> [Expression]
-prepareInput s = plant $ words s
-
-fromPrefixNotation :: String -> Expression
-fromPrefixNotation s = generate $ prepareInput s
-
-fromPostfixNotation :: String -> Expression
-fromPostfixNotation s = generate $ toPrefix (prepareInput s) []
+postfixBuilder :: [Expression] -> Expression
+postfixBuilder s = generate $ toPrefix s []
     where isValue (Value _) = True
           isValue _ = False
           isNode (Node _ _ _) = True
@@ -66,5 +63,18 @@ fromPostfixNotation s = generate $ toPrefix (prepareInput s) []
           toPrefix (a:b) stack
             | isValue a = toPrefix b (a:stack)
             | isNode a = error "invalid postfix expression"
+
+prepareInput :: String -> [Expression]
+prepareInput s = plant $ words s
+
+fromPrefixNotation :: String -> Expression
+fromPrefixNotation s = generate $ prepareInput s
+
+fromPostfixNotation :: String -> Expression
+fromPostfixNotation s = postfixBuilder $ prepareInput s
+
+fromInfixNotation :: String -> Expression
+fromInfixNotation s = postfixBuilder $ toPostfix (prepareInput s)
+    where toPostfix t = t
 
 
