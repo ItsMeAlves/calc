d2bcf0b84ecdba0f3c4e2cd0eed13c808154a1de
Gabriel Alves <itsmealves@gmail.com>
2016-07-26 23:47:25 -0300

    comment app file

--- app.hs
+++ app.hs
@@ -1,12 +1,14 @@
 import Calculate.Expression
 import Options.Applicative
 
+-- Configure args structure
 data Commander = Commander {
     expression :: String,
     prefix :: Bool,
     postfix :: Bool
 }
 
+-- Help parsing args structures
 options :: Parser Commander
 options = Commander 
     <$> strOption
@@ -20,14 +22,16 @@ options = Commander
         (long "postfix"
             <> help "Receives a postfix notation expression and evaluates it")
 
+-- Handle arguments
 optionsHandler :: Commander -> IO ()
 optionsHandler (Commander e True False) = print $ evaluate $ fromPrefixNotation e
 optionsHandler (Commander e False True) = print $ evaluate $ fromPostfixNotation e
 optionsHandler (Commander e True True) = print result 
     where result = (zip ["From prefix: ", "fromPostfixNotation"]
             [evaluate $ fromPrefixNotation e, evaluate $ fromPostfixNotation e])
+optionsHandler (_ False False) = print "Error: Tell me its notation"
 
-
+-- Main function
 main = execParser opts >>= optionsHandler
     where opts = info (helper <*> options) (fullDesc
             <> progDesc "Calculates the result of an expression"
